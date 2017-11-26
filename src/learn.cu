44ddc4639e6352e7ad4f1bd4ce76a87d4141ad8c
Chauncey Wang <chncwang@gmail.com>
2017-11-26 16:46:51 +0800

    test

--- src/learn.cu
+++ src/learn.cu
@@ -76,7 +76,6 @@ float *NewGPUVector(int dim) {
     float *v;
     assert(cudaMalloc((void**)&v, sizeof(float) * dim) == cudaSuccess);
     InitGPUVector(v, dim);
-    GlobalPrintVector<<<1, 1>>>(v, dim);
     cudaDeviceSynchronize();
     return v;
 }
@@ -120,35 +119,75 @@ void N3LDGTanh(float *src, float *dest, int len) {
     N3LDGKernelTanh<<<BlockCount(len) ,THREAD_COUNT_PER_BLOCK>>>(src, dest, len);
 }
 
+__global__ void SingleThreadKernelTanh(float **src, float **dest, int len, int count) {
+    for (int i = 0; i< count; ++i)
+        for (int j = 0; j<len;++j)
+            dest[i][j] = tanh(src[i][j]);
+}
+
+
+void N3LDGSingleThreadTanh(float **src, float **dest, int len, int count) {
+    SingleThreadKernelTanh<<<1, 1>>>(src, dest, len, count);
+}
+
 __global__ void N3LDGKernelTanh(float **src, float **dest, int len, int count) {
-    for (int i = 0; i < count; ++i) {
-        for (int j = 0; j < len; ++j) {
-            dest[i][j] = 12;
+    int thread_count_per_arr = (len / THREAD_COUNT_PER_WRAP + 1) *
+        THREAD_COUNT_PER_WRAP;
+    int index = blockDim.x * blockIdx.x + threadIdx.x;
+    int step = blockDim.x * gridDim.x / thread_count_per_arr;
+    int count_index = index / thread_count_per_arr;
+    for (int i = count_index; i < count; i += step) {
+        int arr_index = index % thread_count_per_arr;
+        if (arr_index < len) {
+            dest[i][arr_index] = tanh(src[i][arr_index]);
         }
     }
-//    int thread_count_per_arr = (len / THREAD_COUNT_PER_WRAP + 1) *
-//        THREAD_COUNT_PER_WRAP;
-//    int index = blockDim.x * blockIdx.x + threadIdx.x;
-//    //printf("thread count per arr:%d\n", thread_count_per_arr);
-//    //printf("index:%d\n", index);
-//    int step = blockDim.x * gridDim.x / thread_count_per_arr;
-//    //printf("step:%d\n", step);
-//    int count_index = index / thread_count_per_arr;
-//    //printf("count_index:%d\n", count_index);
-//    for (int i = count_index; i < count; i += step) {
-//        int arr_index = index % thread_count_per_arr;
-//        //printf("arr index:%d\n", arr_index);
-//        if (arr_index < len) {
-//            dest[i][arr_index] = tanh(src[i][arr_index]);
-//        }
-//    }
 }
+
 
 void N3LDGTanh(float **src, float **dest, int len, int count) {
     assert(len <= MAX_BLOCK_COUNT * THREAD_COUNT_PER_BLOCK);
-    int block_count = BlockCount(len / THREAD_COUNT_PER_WRAP * THREAD_COUNT_PER_WRAP * count);
-    //N3LDGKernelTanh<<<block_count, THREAD_COUNT_PER_BLOCK>>>(src, dest, len, count);
-    N3LDGKernelTanh<<<1, 1>>>(src, dest, len, count);
+    int block_count = BlockCount(((len - 1) / THREAD_COUNT_PER_WRAP + 1) *
+            THREAD_COUNT_PER_WRAP * count);
+    N3LDGKernelTanh<<<block_count, THREAD_COUNT_PER_BLOCK>>>(src, dest, len, count);
+}
+
+__global__ void N3LDGKernelTanhWithoutLimit(float **src, float **dest, int len, int count) {
+    int index = blockDim.x * blockIdx.x + threadIdx.x;
+    int total_count = len * count;
+    int step = blockDim.x * gridDim.x;
+    for (int i = index; i < total_count; i += step) {
+        int ci = i / len;
+        int li = i % len;
+        dest[ci][li] = tanh(src[ci][li]);
+    }
+}
+
+void N3LDGTanhWithoutLimit(float **src, float **dest, int len, int count) {
+    N3LDGKernelTanh<<<BlockCount(len * count), THREAD_COUNT_PER_BLOCK>>>(src, dest, len, count);
+}
+
+__global__ void N3LDGKernelTanhByBlock(float **src, float **dest, int len, int count) {
+    __shared__ volatile float *src_arr;
+    __shared__ volatile float *dest_arr;
+    for (int i = blockIdx.x; i < count; i += gridDim.x) {
+        __syncthreads();
+        if (threadIdx.x == 0) {
+            src_arr = src[i];
+            dest_arr = dest[i];
+        }
+        __syncthreads();
+        for (int j=threadIdx.x; j< len; j += blockDim.x) {
+            dest_arr[j] = tanh(src_arr[j]);
+        }
+    }
+}
+
+void N3LDGTanhByBlock(float **src, float **dest, int len, int count) {
+    N3LDGKernelTanhByBlock<<<min(MAX_BLOCK_COUNT, count), THREAD_COUNT_PER_BLOCK>>>(src, dest, len, count);
+}
+
+void N3LDGKernelTanhByCopy(float **src, float **dest, int len, int count) {
 }
 
 float **ToGpuVectorArray(float** vec, int len) {
@@ -158,4 +197,61 @@ float **ToGpuVectorArray(float** vec, int len) {
     assert(cudaMemcpy(result, vec, size, cudaMemcpyHostToDevice) ==
             cudaSuccess);
     return (float**)result;
+}
+
+float** NewGPUVectors(int count, int dim) {
+    float**  result = (float**)malloc(count * sizeof(float*));
+    for (int i = 0; i<count; ++i) {
+        float * vec = NewGPUVector(dim);
+        result[i] = vec;
+    }
+    return result;
+}
+
+__global__ void KernelAssertEqual(float **a, float **b, int len, int count) {
+    for (int i = 0; i < count; ++i) {
+        for (int j=0; j < len; ++j) {
+            int s = a[i][j] - b[i][j];
+            if(s < -0.001 || s > 0.001) {
+                printf("i:%d, j:%d, a[i][j]:%f, b[i][j]:%f\n", i, j, a[i][j], b[i][j]);
+                assert(false);
+            }
+        }
+    }
+}
+
+void Benchmark() {
+    assert(cudaSetDevice(1) == cudaSuccess);
+    std::vector<int> dims = {100, 1000};
+    std::vector<int> counts = {10, 100, 1000};
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+    float alpha = 1.0;
+    for (auto dim : dims) {
+        for (auto count : counts) {
+            float** gpu_vec_a = NewGPUVectors(count, dim);
+            float** gpu_vec_b = NewGPUVectors(count, dim);
+            float** gpu_vec_c = NewGPUVectors(count, dim);
+            cout << "begin cal" << endl;
+            float sum = 0;
+            int iter = 1000;
+            for (int i = 0; i < iter; ++i) {
+                cudaEvent_t start, stop;
+                cudaEventCreate(&start);
+                cudaEventCreate(&stop);
+                cudaEventRecord(start);
+                float **a = ToGpuVectorArray(gpu_vec_a, count);
+                float **b = ToGpuVectorArray(gpu_vec_b, count);
+                N3LDGTanhByBlock(a, b, dim, count);
+                cudaEventRecord(stop);
+                cudaEventSynchronize(stop);
+                float mill;
+                cudaEventElapsedTime(&mill, start, stop);
+                sum += mill;
+                cudaDeviceSynchronize();
+            }
+            cudaDeviceSynchronize();
+            cout << "dim:" << dim << " count:" <<count << " time:" << sum * 1000 / iter  << endl;
+        }
+    }
 }
